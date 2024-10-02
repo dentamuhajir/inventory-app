e55888c3f3ff16208229dc3f577a2669c30f07db
Denta Muhajir <dentamuhajir@gmail.com>
2024-10-02 20:10:38 +0700

    Add StockTransaction Model

---
+++ src/main/java/com/dentamuhajir/inventory/model/StockTransaction.java
@@ -0,0 +1,40 @@
+package com.dentamuhajir.inventory.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name="stock_transactions")
+public class StockTransaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "stock_transactions_seq")
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "stock_id", nullable = false)
+    private Stock stock;
+
+    @Column(name = "transaction_type", nullable = false)
+    private String transactionType; // e.g., "IN" or "OUT"
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Column(name = "transaction_date", nullable = false)
+    private Date transactionDate;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+}
